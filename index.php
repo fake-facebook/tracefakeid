cc7f03be820217074de77de6c062d407f829390c
gitesh <gitesh.purbiya22@gmail.com>
2014-07-01 00:51:00 +0530

    change index.php file to save data into databse

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-,<html>
+<html>
 <head>
  <title>love</title>
 </head>
 <body>
 <?php
  
-    if (getenv('HTTP_X_FORWARDED_FOR')) {
+ if (getenv('HTTP_X_FORWARDED_FOR')) {
         $pipaddress = getenv('HTTP_X_FORWARDED_FOR');
         $ipaddress = getenv('REMOTE_ADDR');
 echo "Your Proxy IP address is : ".$pipaddress. "(via $ipaddress)" ;
@@ -15,24 +15,17 @@ echo "Your Proxy IP address is : ".$pipaddress. "(via $ipaddress)" ;
 		echo "<br />\n";
     }
 	$ip=ip2long($ipaddress);
-	echo $ip;
-$dbhost = 'localhost:3036';
-$dbuser = 'heroines';
-$dbpass = 'fatkid';
-$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+echo $ip;
+
+// Databse Connectivity
+$conn = mysql_connect('localhost', 'root', '');
 if(! $conn )
 {
   die('Could not connect: ' . mysql_error());
 }
-$sql = 'INSERT INTO IP'. '(IPadd)'. 'VALUES ("$ip")';
+$sql = 'INSERT INTO store_ip'. '(ip_addresses)'. 'VALUES ("$ip")';
 mysql_select_db('test_db');
-$retval = (mql_query ($sql, $conn);
-if(! $retval )
-{
-die ('could not enter data:' . mysql_error());
-
-}
-echo  "entered successfully\n";
+mysql_query($sql,$conn);
 mysql_close($conn);
 ?>
 </body>
